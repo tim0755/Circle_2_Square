eb916cf7c1a283c9be09dab7190d3b65c3f3b0cd
Tim Hu <tim.hu@go6d.com>
2016-09-14 15:42:22 +0800

    add test code from opengl_test_app

--- app/src/main/java/com/example/thinkpad/circle_2_square/MainActivity.java
+++ app/src/main/java/com/example/thinkpad/circle_2_square/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.thinkpad.circle_2_square;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,6 +12,7 @@ import java.io.FileOutputStream;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "calcArray";
+    private static final boolean DEBUG = true;    // TODO set false on release
 
     private final int W = 960;
     private final int H = 960;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     private final int centerY = 480;
 
     private CPoint[][] tableArray;
+
+    private byte[] mUvcCameraData0_;
 
     private int number = 0;
 
@@ -61,6 +65,37 @@ public class MainActivity extends AppCompatActivity {
         }*/
 
     }
+
+    /*private void circle2Square(byte[] src) {
+        int offset = 0;
+        int yuvOffset = W * H;
+        if (DEBUG)Log.v(TAG, "circle2Square start!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1");
+        for (int y = 0; y < H; y++) {
+            for (int x = 0; x < W; x++) {
+                if ((int) Math.pow(Math.pow(x - centerX, 2.0) + Math.pow(y - centerY, 2.0), 1 / 2.0) > radius)
+                    continue;
+                mUvcCameraData0_[tableArray[y][x].v * W + tableArray[y][x].u] = src[offset + x];
+                if (y % 2 == 0 && x % 2 == 0) {
+                    if((tableArray[y][x].v / 2) * W + tableArray[y][x].u / 2 >= yuvOffset/2)
+                        Log.v(TAG, "x*y:"+x+"*"+y+" u*v:"+tableArray[y][x].u+"*"+tableArray[y][x].v+" circle2Square "+((tableArray[y][x].v / 2) * W + tableArray[y][x].u / 2)+" "+(yuvOffset/2));
+                    else {
+                        mUvcCameraData0_[yuvOffset + W * (tableArray[y][x].v / 2) + tableArray[y][x].u / 2]     = src[yuvOffset + W * (y / 2) + x / 2];
+                        mUvcCameraData0_[yuvOffset + W * (tableArray[y][x].v / 2) + tableArray[y][x].u / 2 + 1] = src[yuvOffset + W * (y / 2) + x / 2 + 1];
+                    }
+                }
+            }
+            offset += W;
+        }
+
+        {
+            if (DEBUG)Log.v(TAG, "circle2Square done!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!0");
+            Mat frameChain = new Mat(mPreviewHeight + (mPreviewHeight / 2), mPreviewWidth, CvType.CV_8UC1);
+            frameChain.put(0, 0, mUvcCameraData0_);
+            Mat srcMat = new Mat();
+            Imgproc.cvtColor(frameChain, srcMat, Imgproc.COLOR_YUV2BGRA_NV21, 4);
+            Imgcodecs.imwrite(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath() + "/uvc_camera_2_square_snapshot_one_rgba.jpg", srcMat);
+        }
+    }*/
 
     private class CPoint {
         public int u;
